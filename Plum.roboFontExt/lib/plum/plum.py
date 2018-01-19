d80a5e7c5876f3d511e884e40aaf3f6a3b2e0e60
typoman <eslami.bahman@gmail.com>
2018-01-19 10:51:15 +0100

    Update for Robofont 2.0.

--- Plum.roboFontExt/lib/plum/plum.py
+++ Plum.roboFontExt/lib/plum/plum.py
@@ -1,59 +1,120 @@
+import mojo.roboFont
+ROBOFONTMAINVER = int(mojo.roboFont.version[0])
+
 GUIDE_NAME = 'plum'
 
+if ROBOFONTMAINVER == 2:
+    class Plum(object):
 
-class Plum(object):
+        def __init__(self, glyph):
+            self.glyph = glyph
 
-    def __init__(self, glyph):
-        self.glyph = glyph
+        def toggle(self):
+            if self.plum_guide:
+                self.destroy()
+            else:
+                self.create()
 
-    def toggle(self):
-        if self.plum_guide:
-            self.destroy()
-        else:
-            self.create()
+        def create(self):
+            if self.glyph_exists:
+                self.glyph.appendGuideline(self.position, self.angle, name=GUIDE_NAME)
 
-    def create(self):
-        if self.glyph_exists:
-            self.glyph.addGuide(self.position, self.angle, name=GUIDE_NAME)
+        def destroy(self):
+            if self.glyph_exists:
+                self.glyph.removeGuide(self.plum_guide)
 
-    def destroy(self):
-        if self.glyph_exists:
-            self.glyph.removeGuide(self.plum_guide)
+        def update(self):
+            if self.glyph_exists:
+                guide = self.plum_guide
+                if guide:
+                    guide.x = self.horizontal_center
 
-    def update(self):
-        if self.glyph_exists:
-            guide = self.plum_guide
-            if guide:
-                guide.x = self.horizontal_center
+        @property
+        def guides(self):
+            return self.glyph.guidelines if self.glyph_exists else []
 
-    @property
-    def guides(self):
-        return self.glyph.guides if self.glyph_exists else []
+        @property
+        def plum_guides(self):
+            return (guide for guide in self.guides if guide.name is GUIDE_NAME)
 
-    @property
-    def plum_guides(self):
-        return (guide for guide in self.guides if guide.name is GUIDE_NAME)
+        @property
+        def plum_guide(self):
+            return next(self.plum_guides, None)
 
-    @property
-    def plum_guide(self):
-        return next(self.plum_guides, None)
+        @property
+        def angle(self):
+            return 90 + (self.font.info.italicAngle or 0)
 
-    @property
-    def angle(self):
-        return 90 + (self.font.info.italicAngle or 0)
+        @property
+        def position(self):
+            return (self.glyph.width / 2, 0)
 
-    @property
-    def position(self):
-        return (self.glyph.width / 2, 0)
+        @property
+        def horizontal_center(self):
+            return self.position[0]
 
-    @property
-    def horizontal_center(self):
-        return self.position[0]
+        @property
+        def font(self):
+            return self.glyph.getParent()
 
-    @property
-    def font(self):
-        return self.glyph.getParent()
+        @property
+        def glyph_exists(self):
+            return self.glyph is not None
 
-    @property
-    def glyph_exists(self):
-        return self.glyph is not None
+elif ROBOFONTMAINVER == 1:
+    class Plum(object):
+
+        def __init__(self, glyph):
+            self.glyph = glyph
+
+        def toggle(self):
+            if self.plum_guide:
+                self.destroy()
+            else:
+                self.create()
+
+        def create(self):
+            if self.glyph_exists:
+                self.glyph.addGuide(self.position, self.angle, name=GUIDE_NAME)
+
+        def destroy(self):
+            if self.glyph_exists:
+                self.glyph.removeGuide(self.plum_guide)
+
+        def update(self):
+            if self.glyph_exists:
+                guide = self.plum_guide
+                if guide:
+                    guide.x = self.horizontal_center
+
+        @property
+        def guides(self):
+            return self.glyph.guides if self.glyph_exists else []
+
+        @property
+        def plum_guides(self):
+            return (guide for guide in self.guides if guide.name is GUIDE_NAME)
+
+        @property
+        def plum_guide(self):
+            return next(self.plum_guides, None)
+
+        @property
+        def angle(self):
+            return 90 + (self.font.info.italicAngle or 0)
+
+        @property
+        def position(self):
+            return (self.glyph.width / 2, 0)
+
+        @property
+        def horizontal_center(self):
+            return self.position[0]
+
+        @property
+        def font(self):
+            return self.glyph.getParent()
+
+        @property
+        def glyph_exists(self):
+            return self.glyph is not None
